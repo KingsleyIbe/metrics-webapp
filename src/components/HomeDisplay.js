9a040a145151a2846bd667eba4920b25b4579da3
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-02-02 13:59:44 +0100

    Add some styles

--- src/components/HomeDisplay.js
+++ src/components/HomeDisplay.js
@@ -14,7 +14,7 @@ const HomeDisplay = () => {
   }, []);
 
   const covidCases = covidLists.map((cases) => (
-    <div className="country-name" id="country-name" key={cases.code}>
+    <div className="country-container" id="country-name" key={cases.code}>
       <div className="country-c">
         <Link className="country-d" to={`/${cases.code}`}>
           {cases.name}
@@ -29,8 +29,7 @@ const HomeDisplay = () => {
 
   return (
     <>
-      <div className="country-name">Active Corona Cases </div>
-      <input className="search" type="text" id="input-country" placeholder="Country name.." />
+      <div className="country-name"><h2>Active Corona Cases </h2></div>
       <div className="countries-con">
         {covidCases}
       </div>
